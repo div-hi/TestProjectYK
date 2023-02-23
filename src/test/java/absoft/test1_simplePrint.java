56d82eab9f93520dca2c595190877ba6c067468a
yana lavrinenko <lavrinenko.y.al@gmail.com>
2023-02-23 13:59:14 +0100

    Completed test1

--- src/test/java/absoft/test1_simplePrint.java
+++ src/test/java/absoft/test1_simplePrint.java
@@ -9,10 +9,18 @@ Hello (name)
 Your age is (age)
  */
 
+import java.util.Scanner;
+
 public class test1_simplePrint {
 
     public static void main (String [] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+        System.out.println("Enter your age");
+        int age = sc.nextInt();
+        System.out.println("Hello " + name);
+        System.out.println("Your age is " + age);
     }
 }
 
