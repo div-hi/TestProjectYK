d8299d84724d9c48f6811a1b7eea9bd28992a71f
yana lavrinenko <lavrinenko.y.al@gmail.com>
2023-02-23 16:22:57 +0100

    Completed test5

--- src/test/java/absoft/test5_Operators_3.java
+++ src/test/java/absoft/test5_Operators_3.java
@@ -6,6 +6,30 @@ package absoft;
 Используйте конструкцию if-else-if
  */
 
+import java.util.Scanner;
+
 public class test5_Operators_3 {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int number = sc.nextInt();
+        if (number == 1){
+            System.out.println("Понедельник");
+        } else if (number == 2){
+            System.out.println("Вторник");
+        } else if (number == 3){
+            System.out.println("Среда");
+        } else if (number == 4){
+            System.out.println("Четверг");
+        } else if (number == 5){
+            System.out.println("Пятница");
+        } else if (number == 6){
+            System.out.println("Суббота");
+        } else if (number == 7){
+            System.out.println("Воскресенье");
+        } else {
+            System.out.println("Ошибка, введите число от 1 до 7");
+        }
+    }
 
 }
