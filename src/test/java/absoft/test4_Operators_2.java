75e0f43924324cca63e30a49ed3a0b97a6403f55
yana lavrinenko <lavrinenko.y.al@gmail.com>
2023-02-23 15:56:13 +0100

    Completed test4

--- src/test/java/absoft/test4_Operators_2.java
+++ src/test/java/absoft/test4_Operators_2.java
@@ -6,6 +6,40 @@ package absoft;
 Используйте конструкцию switch - case
  */
 
+import java.util.Scanner;
+
 public class test4_Operators_2 {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int number = sc.nextInt();
+        switch(number){
+            case 1:
+                System.out.println("Понедельник");
+                break;
+            case 2:
+                System.out.println("Вторник");
+                break;
+            case 3:
+                System.out.println("Среда");
+                break;
+            case 4:
+                System.out.println("Четверг");
+                break;
+            case 5:
+                System.out.println("Пятница");
+                break;
+            case 6:
+                System.out.println("Суббота");
+                break;
+            case 7:
+                System.out.println("Воскресенье");
+                break;
+            default:
+                System.out.println("Ошибка, введите число от 1 до 7");
+                break;
+        }
+    }
+
 
 }
