4a59986dc3f11fd7cc844a57d98b819c6fa0574b
yana lavrinenko <lavrinenko.y.al@gmail.com>
2023-02-23 18:10:13 +0100

    Completed test9

--- src/test/java/absoft/test8_for_2.java
+++ src/test/java/absoft/test8_for_2.java
@@ -5,6 +5,19 @@ package absoft;
 Если введенное пользователем число (n) меньше 1 или больше 20 - вывести текст "Ошибка, введите число от 1 до 20"
  */
 
-public class test8_for_2 {
+import java.util.Scanner;
 
-}
+public class test8_for_2 {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+            if (n < 1 || n > 20){
+             System.out.println("Ошибка, введите число от 1 до 20");
+             System.exit(1);
+        }
+            for (int x = 1; x <= n; x++ ){
+             System.out.println(x);
+        }
+        sc.close();
+        }
+    }
