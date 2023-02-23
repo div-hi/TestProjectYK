08c2c42fa9405a84ad6638364749a4501c168755
yana lavrinenko <lavrinenko.y.al@gmail.com>
2023-02-23 17:35:32 +0100

    Completed test6

--- src/test/java/absoft/test6_for.java
+++ src/test/java/absoft/test6_for.java
@@ -4,5 +4,10 @@ package absoft;
  */
 
 public class test6_for {
+    public static void main (String [] args){
+        for (int x = 1; x < 6; x++) {
+            System.out.println(x);
+        }
+    }
 
 }
