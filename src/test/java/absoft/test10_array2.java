8c08f5648068543894669be74d5992ac1c9ba82b
yana lavrinenko <lavrinenko.y.al@gmail.com>
2023-02-24 17:28:17 +0100

    Completed test10

--- src/test/java/absoft/test10_array2.java
+++ src/test/java/absoft/test10_array2.java
@@ -7,6 +7,17 @@ package absoft;
 Выведите в стобик все елементы массива (каждое имя должно быть с новой строки)
  */
 
-public class test10_array2 {
+import java.util.Arrays;
 
+public class test10_array2 {
+    public static void main (String [] args) {
+        String[] array = {"John", "Dan", "Ashe", "Carl", "Bill"};
+        String[] newArray = new String[array.length + 1];
+        Arrays.sort(array);
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        newArray[newArray.length - 1] = "Stan";
+        for (String s : newArray) {
+            System.out.println(s);
+        }
+    }
 }
